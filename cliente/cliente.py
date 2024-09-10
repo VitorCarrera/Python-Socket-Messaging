91b99256f112731ec0daad123f08c31fecfe95cf
Vitor Carrera <vitorcarreraya@gmail.com>
2024-09-10 17:11:42 -0300

    Criando o método enviar_mensagem no arquivo cliente.py

--- cliente/cliente.py
+++ cliente/cliente.py
@@ -1,9 +1,16 @@
 import socket
 
+def enviar_mensagem(cliente_socket, mensagem):
+    cliente_socket.sendall(mensagem.encode())
+    data = cliente_socket.recv(1024)
+    print(f"Resposta do servidor: {data:decode()}")
+
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Conectando ao servidor
 client_socket.connect(('localhost', 12345))
+
+
 
 # Enviando um mensagem para o servidor
 message = "Olá, servidor!!"
