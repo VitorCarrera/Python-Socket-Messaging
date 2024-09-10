038c093fa10c3ca04ed9aaaf8751bedd0f0e2507
Vitor Carrera <vitorcarreraya@gmail.com>
2024-09-10 17:01:07 -0300

    Criando método gerar_resposta no arquivo servidor.py

--- servidor/servidor.py
+++ servidor/servidor.py
@@ -1,5 +1,18 @@
 import socket
 
+def gerar_resposta(mensagem):
+    if "olá" in mensagem.lower():
+        return "Olá, cliente! Como posso ajudar?"
+    elif "Como você está?" in mensagem.lower():
+        return "Sou apenas um servidor"
+    elif "Qual é a melhor linguagem de programação?" in mensagem.lower():
+        return "A melhor linguagem de programação é o C#"
+    elif "encerrar" or "saiu" in mensagem.lower():
+        return "Encerrando conexão!"
+    else: 
+        return "Não entendi sua mensagem"
+
+
 # Criando um socket TCP/IP
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Vinculando o socket ao endereço e porta
@@ -12,13 +25,13 @@ print("Servidor escutando na porta...")
 # Aceitando conexões de clientes
 while True:
     conn, addr = server_socket.accept()
-
     print(f"Conectado por {addr}")
 
-    # Recebendo mensagem do cliente
-    data = conn.recv(1024)
-    print(f"Mensagem recebida: {data.decode()}")
+    while True: 
+        # Recebendo mensagem do cliente
+        data = conn.recv(1024)
+        print(f"Mensagem recebida: {data.decode()}")
 
-    # Respondendo ao cliente
-    conn.sendall(b"Mensagem recebida pelo servidor")
-    conn.close()
+        # Respondendo ao cliente
+        conn.sendall(b"Mensagem recebida pelo servidor")
+        conn.close()
